ff869559f8feefba81c97f8d7118f4ac3e93412f
Runar Sveinsson <runar@nextgl.com>
2022-08-18 09:34:09 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
+plugins {
+  java
+  application
+  id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+group = "com.example"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+  mavenLocal()
+  mavenCentral()
+}
+
+val vertxVersion = "4.3.2"
+val junitJupiterVersion = "5.7.0"
+
+val mainVerticleName = "com.example.starter.MainVerticle"
+val launcherClassName = "io.vertx.core.Launcher"
+
+val watchForChange = "src/**/*"
+val doOnChange = "${projectDir}/gradlew classes"
+
+application {
+  mainClass.set(launcherClassName)
+}
+
+dependencies {
+  implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
+  implementation("io.vertx:vertx-web")
+
+
+  implementation("io.reactiverse:reactiverse-contextual-logging:1.2.0-SNAPSHOT")
+  
+  implementation("org.slf4j:slf4j-api:1.7.35")
+  implementation("ch.qos.logback:logback-core:1.2.11")
+  implementation("ch.qos.logback:logback-classic:1.2.11")  
+  testImplementation("io.vertx:vertx-junit5")
+  testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<ShadowJar> {
+  archiveClassifier.set("fat")
+  manifest {
+    attributes(mapOf("Main-Verticle" to mainVerticleName))
+  }
+  mergeServiceFiles()
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+  testLogging {
+    events = setOf(PASSED, SKIPPED, FAILED)
+  }
+}
+
+tasks.withType<JavaExec> {
+  args = listOf("run", mainVerticleName, "--redeploy=$watchForChange", "--launcher-class=$launcherClassName", "--on-redeploy=$doOnChange")
+  systemProperty("vertx.logger-delegate-factory-class-name","io.vertx.core.logging.JULLogDelegateFactory")
+}
